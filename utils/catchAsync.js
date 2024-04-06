661c44e502f9ad925341ef8e611ece3d38011763
Sabbir Hossain <sabbir2always@gmail.com>
2024-04-06 16:36:50 +0600

    added : catchasync for handling async error on the controller

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,5 +1,7 @@
 module.exports = catchAsync = function (fn) {
-  return (req) => {
-    console.log(req);
+  // the reason this return function get's access to the req,res,next is this is the middleware function
+  // that return a functtion with this object that being called when client hit it's targeted route
+  return (req, res, next) => {
+    fn(req, res, next).catch((err) => next(err));
   };
 };
