fc742524ad030ac1997a14d13091356ddc3b8db2
Sabbir Hossain <sabbir2always@gmail.com>
2024-04-08 16:51:05 +0600

    Booking schema added

--- models/Booking.js
+++ models/Booking.js
@@ -1,10 +1,32 @@
 const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema(
-  {},
+  {
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      require: [true, "A booking must have a tour id"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      require: [true, "A booking must have a user id"],
+    },
+    price: {
+      type: Number,
+      require: [true, "A booking must have a price"],
+    },
+    isPaid: {
+      type: Boolean,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
 const bookingModel = mongoose.model("Booking", bookingSchema);
 
-module.exports = bookingm;
+module.exports = bookingModel;
