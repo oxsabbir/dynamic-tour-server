f72095d084319dd0d2ae0ad3a51403f839700c98
Sabbir Hossain <sabbir2always@gmail.com>
2024-04-08 16:46:06 +0600

    Review shcema added

--- models/Review.js
+++ models/Review.js
@@ -1,7 +1,35 @@
 const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema(
-  {},
+  {
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      require: [true, "A review must have tour id"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      require: [true, "A review must have a user id"],
+    },
+    rating: {
+      type: Number,
+      require: true,
+      min: 1,
+      max: 5,
+    },
+    review: {
+      type: String,
+      require: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    updatedAt: {
+      type: Date,
+    },
+  },
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
