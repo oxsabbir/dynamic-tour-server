e71ee48020b9cec01cf402d1985960b43e4aaa12
Sabbir Hossain <sabbir2always@gmail.com>
2024-04-17 09:58:18 +0600

    added signup route and signup controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,9 +4,31 @@ const catchAsync = require("../utils/catchAsync");
 // Sign up - Create new account
 exports.signUp = catchAsync(async function (req, res, next) {
   // validate the requestBody remove the role if user send it
+  if (req.body.role) {
+    req.body.role = undefined;
+  }
+  // if user try to put the createdAt property
+  if (req.body.createdAt) {
+    req.body.createdAt = Date.now();
+  }
   // check if user already exist
+  const userExist = await User.findOne({ email: req.body.email });
+  if (userExist) next("User already exist using this email");
+
+  // this things handled from the user model
   // create encrypts password
   // save password in the database
+
+  // create the user
+  const newUser = await User.create(req.body);
+
+  res.status(200).json({
+    status: "success",
+    message: "User created successfully",
+    data: {
+      user: newUser,
+    },
+  });
   // generate JSONWEBTOKEN
   // send the response
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -16,6 +16,7 @@ exports.getAllUser = catchAsync(async function (req, res, next) {
   const allUser = await User.find();
   res.status(200).json({
     status: "success",
+    message: "User data retrive successfully",
     data: {
       total: allUser.length,
       user: allUser,
