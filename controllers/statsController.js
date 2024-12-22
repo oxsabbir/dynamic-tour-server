e506fd79139146ade05bb8476c42f7f9ea7a5506
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-22 09:53:56 +0600

    add data output demo to sales

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -1,8 +1,30 @@
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/AppError");
+const Booking = require("../models/Booking");
 
 exports.getSalesStats = catchAsync(async function (req, res, next) {
+  const filterType = req.query?.filter || "month";
+
   res.status(200).json({
     status: "success",
+    filter: filterType,
+
+    data: {
+      totalSells: {
+        amount: 234532,
+        rate: 23,
+        increased: false,
+      },
+      averageSells: {
+        amount: 234532,
+        rate: 120,
+        increased: true,
+      },
+      totalBookings: {
+        amount: 213,
+        rate: 5,
+        increased: true,
+      },
+    },
   });
 });
