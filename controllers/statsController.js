18ca2edd0c47627ccad93a2381c30a6a79e465c5
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-23 08:47:36 +0600

    sell report completed

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -3,9 +3,10 @@ const AppError = require("../utils/AppError");
 const Booking = require("../models/Booking");
 
 exports.getSalesStats = catchAsync(async function (req, res, next) {
-  const filterType = req.query?.filter || "month";
+  const filterType = req.query?.filter || null;
 
   if (
+    req.query.filter &&
     req.query?.filter !== "today" &&
     req.query?.filter !== "week" &&
     req.query?.filter !== "month" &&
@@ -29,11 +30,17 @@ exports.getSalesStats = catchAsync(async function (req, res, next) {
 
   const dayInMillS = day * 24 * 60 * 60 * 1000;
 
-  const filterDate = new Date(Date.now() - dayInMillS).toISOString();
-  console.log(filterDate);
+  let filterDate = null;
+  if (filterType !== null) {
+    filterDate = new Date(Date.now() - dayInMillS).toISOString();
+  }
 
-  const boookingSales = await Booking.aggregate([
-    { $match: { createdAt: { $lt: new Date() } } },
+  const [boookingSales] = await Booking.aggregate([
+    {
+      $match: {
+        createdAt: filterDate ? { $gt: new Date(filterDate) } : { $ne: false },
+      },
+    },
     {
       $group: {
         _id: null,
@@ -44,7 +51,7 @@ exports.getSalesStats = catchAsync(async function (req, res, next) {
     },
   ]);
 
-  console.log(boookingSales);
+  // trying in the past
 
   res.status(200).json({
     status: "success",
@@ -52,17 +59,17 @@ exports.getSalesStats = catchAsync(async function (req, res, next) {
 
     data: {
       totalSells: {
-        amount: 234532,
+        amount: boookingSales?.totalSells || 0,
         rate: 23,
         increased: false,
       },
       averageSells: {
-        amount: 234532,
+        amount: boookingSales?.averageSells || 0,
         rate: 120,
         increased: true,
       },
       totalBookings: {
-        amount: 213,
+        amount: boookingSales?.totalBookings || 0,
         rate: 5,
         increased: true,
       },
